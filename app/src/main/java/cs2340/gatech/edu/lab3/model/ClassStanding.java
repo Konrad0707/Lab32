c31a2dd0d18c459f6c2d5f013ad9c1bece66b307
Konrad Wang <4321konrad@gmail.com>
2018-02-06 01:59:54 -0500

    import

---
+++ app/src/main/java/cs2340/gatech/edu/lab3/model/ClassStanding.java
@@ -0,0 +1,14 @@
+package cs2340.gatech.edu.lab3.model;
+import java.io.Serializable;
+/**
+ * Created by Konrad Wang on 2/6/2018.
+ */
+
+public enum ClassStanding implements Serializable {
+    FR,
+    SO,
+    JR,
+    SR;
+
+
+}
